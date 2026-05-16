896dc8de5908952dcee910a11f372cc518c0996e
vishwaveerabathini-gif <vishwaveerabathini@gmail.com>
2026-05-16 16:47:18 +0530

    project fixed

--- public/leaderboard.js
+++ public/leaderboard.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-async function loadLeaderboard() {
-
-    try {
-
-        fetch("https://creative-coding-play-ground.onrender.com/rankings")
-
-        let users = await res.json();
-
-        let output = "";
-
-        users.forEach((user, index) => {
-
-            output += `
-            <div class="row">
-
-                <div>
-                    #${index + 1}
-                </div>
-
-                <div>
-                    ${user.username}
-                </div>
-
-                <div>
-                    ${user.points} pts
-                </div>
-
-            </div>
-            `;
-        });
-
-        document.getElementById("leaderboard").innerHTML = output;
-
-    } catch (err) {
-
-        console.log("Leaderboard error");
-
-    }
-
-}
-
-=======
 async function loadLeaderboard() {
 
     try {
@@ -82,5 +39,4 @@ async function loadLeaderboard() {
 
 }
 
->>>>>>> a414f257ee4008c4bf969ab87ceea62acde97535
 loadLeaderboard();
--- public/learn.js
+++ public/learn.js
@@ -1,32 +1,15 @@
-<<<<<<< HEAD
-// function checkCode() {
-//     let code = document.getElementById("editor").value;
-//     let result = document.getElementById("result");
+function checkCode() {
+    let code = document.getElementById("editor").value;
+    let result = document.getElementById("result");
 
-//     try {
-//         eval(code);
+    try {
+        eval(code);
 
-//         result.innerHTML = "✅ Code is Correct";
-//         result.style.color = "green";
-//     }
-//     catch(error) {
-//         result.innerHTML = "❌ Code is Wrong <br>💡 " + error.message;
-//         result.style.color = "red";
-//     }
-=======
-// function checkCode() {
-//     let code = document.getElementById("editor").value;
-//     let result = document.getElementById("result");
-
-//     try {
-//         eval(code);
-
-//         result.innerHTML = "✅ Code is Correct";
-//         result.style.color = "green";
-//     }
-//     catch(error) {
-//         result.innerHTML = "❌ Code is Wrong <br>💡 " + error.message;
-//         result.style.color = "red";
-//     }
->>>>>>> a414f257ee4008c4bf969ab87ceea62acde97535
-// }
+        result.innerHTML = "✅ Code is Correct";
+        result.style.color = "green";
+    }
+    catch(error) {
+        result.innerHTML = "❌ Code is Wrong <br>💡 " + error.message;
+        result.style.color = "red";
+    }
+}
